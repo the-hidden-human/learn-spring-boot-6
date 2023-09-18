e55f12876350764a7f04a9aead148c8e42bf69b8
The Hidden Human <the.hidden.human@outlook.com>
2023-09-18 11:48:59 +0700

    Full Successfully

--- springboot-part1/src/main/java/com/h2hm/springbootpart1/controller/ManagementController.java
+++ springboot-part1/src/main/java/com/h2hm/springbootpart1/controller/ManagementController.java
@@ -24,18 +24,22 @@ public class ManagementController {
             }
 
     )
+
     @GetMapping
     public String get() {
         return "GET:: management controller";
     }
+
     @PostMapping
     public String post() {
         return "POST:: management controller";
     }
+
     @PutMapping
     public String put() {
         return "PUT:: management controller";
     }
+
     @DeleteMapping
     public String delete() {
         return "DELETE:: management controller";
